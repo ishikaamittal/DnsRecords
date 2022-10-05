0530bc7aa356f9d3d333ebb13d6be658e41de99f
Ishika <mittalishika1801@gmail.com>
2022-10-05 18:20:36 +0530

    get/post

--- APIs/views.py
+++ APIs/views.py
@@ -4,6 +4,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
+
+from symbol import dotted_as_name
 from .serializers import DnsSerializer
 from .models import DnsRecords
 import socket
@@ -23,18 +25,18 @@ class RecordView(APIView):
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def get(self, request, uuid=None):
-        if uuid:
-            domain = DnsRecords.objects.get(uuid=uuid)
-            # ip = DnsRecords.objects.get(ip)
-            # ip= socket.gethostbyname(domain)
-            serializer = DnsSerializer(domain)
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
- 
-        students = DnsRecords.objects.all()
-        serializer = DnsSerializer(students, many=True)
-        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK) 
-
+    def get(self, request,domain=None):
+        domain = DnsRecords.objects.get(domain=domain)
+        if(request.method == 'GET'):
+            if(domain):
+                records = DnsRecords.objects.all()
+                serializer = DnsSerializer(records, many=True)
+                return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK) 
+            else:
+                ip = socket.gethostbyname(domain)
+                
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        
     # def get(self, request):
     #     if(request.method == 'GET'):
     #         # r = DnsRecords.objects.all().values().order_by('domain')
@@ -53,3 +55,4 @@ class RecordView(APIView):
     #             return Response({"response": f"{domain} doesn't exist"})
     #     else:
     #         return Response(status=status.HTTP_404_NOT_FOUND)
+
