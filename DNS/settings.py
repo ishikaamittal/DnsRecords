0b6dc9803ad3cd5df2658f46a2e0b01b8dc88c95
Ishika <mittalishika1801@gmail.com>
2022-10-06 16:12:27 +0530

    update

--- DNS/settings.py
+++ DNS/settings.py
@@ -136,6 +136,6 @@ REST_FRAMEWORK = {
 }
 
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# import dj_database_url 
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
